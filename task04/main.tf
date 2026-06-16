4b89612877c7baf5418dd4aa285acf913da4b246
Lilit <galstyan.lilit.00@gmail.com>
2026-06-17 00:09:13 +0400

    Added task 4

---
+++ task04/main.tf
@@ -0,0 +1,143 @@
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name  = var.resource_group_name
+  location = var.location
+  tags  = var.tags
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name  = var.vnet_name
+  address_space  = var.vnet_address_space
+  location  = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = var.tags
+}
+
+# Subnet (standalone resource)
+resource "azurerm_subnet" "frontend" {
+  name = var.subnet_name
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes  = var.subnet_address_prefixes
+}
+
+# Public IP
+resource "azurerm_public_ip" "pip" {
+  name = var.public_ip_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  allocation_method  = "Static"
+  sku = "Standard"
+  domain_name_label = var.dns_name_label
+  tags = var.tags
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name = var.nsg_name
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = var.tags
+}
+
+# NSG Rules (standalone resources)
+resource "azurerm_network_security_rule" "allow_http" {
+  name = var.nsg_http_rule_name
+  priority = 100
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "Tcp"
+  source_port_range = "*"
+  destination_port_range = "80"
+  source_address_prefix = "*"
+  destination_address_prefix  = "*"
+  resource_group_name  = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name = var.nsg_ssh_rule_name
+  priority = 110
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "Tcp"
+  source_port_range = "*"
+  destination_port_range = "22"
+  source_address_prefix  = "*"
+  destination_address_prefix = "*"
+  resource_group_name = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name = var.nic_name
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = var.tags
+
+  ip_configuration {
+    name = "internal"
+    subnet_id  = azurerm_subnet.frontend.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+# NIC
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  size = var.vm_size
+  admin_username = var.vm_admin_username
+  admin_password = var.vm_password
+  tags = var.tags
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer = "ubuntu-24_04-lts"
+    sku = "server"
+    version = "latest"
+  }
+
+  
+  depends_on = [
+    azurerm_network_interface_security_group_association.nic_nsg
+  ]
+
+  connection {
+    type = "ssh"
+    host = azurerm_public_ip.pip.ip_address
+    user = var.vm_admin_username
+    password = var.vm_password
+    timeout = "5m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y nginx",
+      "sudo systemctl enable nginx",
+      "sudo systemctl start nginx",
+    ]
+  }
+}
---
+++ task04/terraform.tfvars
@@ -0,0 +1,18 @@
+location = "West Europe"
+resource_group_name = "cmaz-iaco50xm-mod4-rg"
+vnet_name = "cmaz-iaco50xm-mod4-vnet"
+subnet_name = "frontend"
+nic_name = "cmaz-iaco50xm-mod4-nic"
+nsg_name = "cmaz-iaco50xm-mod4-nsg"
+nsg_http_rule_name = "AllowHTTP"
+nsg_ssh_rule_name = "AllowSSH"
+public_ip_name = "cmaz-iaco50xm-mod4-pip"
+dns_name_label  = "cmaz-iaco50xm-mod4-nginx"
+vm_name  = "cmaz-iaco50xm-mod4-vm"
+vm_os_version = "ubuntu-24_04-lts"
+vm_size = "Standard_B2s_v2"
+vm_admin_username = "azureuser"
+
+tags = {
+  Creator = "lilit_galstyan@epam.com"
+}
