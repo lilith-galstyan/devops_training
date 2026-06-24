2cd8e0606ed3b93e37ecd13e584f03a7b7f19b59
Lilit <galstyan.lilit.00@gmail.com>
2026-06-24 18:09:54 +0400

    Fixed versions, added descriptions for outputs

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,17 +1,21 @@
 output "sql_server_name" {
-  value = azurerm_mssql_server.server.name
+  description = "The name of the Azure SQL Server"
+  value       = azurerm_mssql_server.server.name
 }
 
 output "admin_password" {
-  sensitive = true
-  value     = local.admin_password
+  description = "The generated SQL admin password"
+  sensitive   = true
+  value       = local.admin_password
 }
 
 output "sql_server_fqdn" {
-  value = azurerm_mssql_server.server.fully_qualified_domain_name
+  description = "The FQDN of the Azure SQL Server"
+  value       = azurerm_mssql_server.server.fully_qualified_domain_name
 }
 
 output "sql_connection_string" {
-  sensitive = true
-  value     = "Server=tcp:${azurerm_mssql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${var.admin_username};Password=${local.admin_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  description = "The connection string for the Azure SQL Database"
+  sensitive   = true
+  value       = "Server=tcp:${azurerm_mssql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};Persist Security Info=False;User ID=${var.admin_username};Password=${local.admin_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
--- task06/outputs.tf
+++ task06/outputs.tf
@@ -1,7 +1,9 @@
 output "app_hostname" {
-  value = module.webapp.app_hostname
+  description = "The hostname of the Azure Linux Web App"
+  value       = module.webapp.app_hostname
 }
 
 output "sql_server_fqdn" {
-  value = module.sql.sql_server_fqdn
+  description = "The FQDN of the Azure SQL Server"
+  value       = module.sql.sql_server_fqdn
 }
