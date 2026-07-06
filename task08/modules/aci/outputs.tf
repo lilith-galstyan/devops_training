23168587cdfdd61b1b922d12086a662d3298eb00
Lilit <galstyan.lilit.00@gmail.com>
2026-07-06 16:59:41 +0400

    Fixed terraform version and added descriptions for outputs

--- task08/modules/aci/outputs.tf
+++ task08/modules/aci/outputs.tf
@@ -1,3 +1,4 @@
 output "aci_fqdn" {
   value = azurerm_container_group.container.fqdn
+  description = "The fully qualified domain name (FQDN) of the Azure Container Instance (ACI)."
 }
--- task08/modules/acr/outputs.tf
+++ task08/modules/acr/outputs.tf
@@ -1,16 +1,20 @@
 output "login_server" {
   value = azurerm_container_registry.acr.login_server
+  description = "The login server of the Azure Container Registry (ACR)."
 }
 
 output "id" {
   value = azurerm_container_registry.acr.id
+  description = "The ID of the Azure Container Registry (ACR)."
 }
 
 output "admin_username" {
   value = azurerm_container_registry.acr.admin_username
+  description = "The admin username of the Azure Container Registry (ACR)."
 }
 
 output "admin_password" {
   value     = azurerm_container_registry.acr.admin_password
+  description = "The admin password of the Azure Container Registry (ACR)."
   sensitive = true
 }
