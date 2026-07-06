a9c079ecb3e7c6fd8717f95fe57aaa14ad3b6e2e
Lilit <galstyan.lilit.00@gmail.com>
2026-07-06 17:05:26 +0400

    Formatted code

--- task08/outputs.tf
+++ task08/outputs.tf
@@ -1,9 +1,9 @@
 output "aci_fqdn" {
-  value = module.aci.aci_fqdn
+  value       = module.aci.aci_fqdn
   description = "The fully qualified domain name (FQDN) of the Azure Container Instance (ACI)."
 }
 
 output "aks_lb_ip" {
-  value = data.kubernetes_service.app_service.status[0].load_balancer[0].ingress[0].ip
+  value       = data.kubernetes_service.app_service.status[0].load_balancer[0].ingress[0].ip
   description = "The public IP address of the AKS Load Balancer."
 }
