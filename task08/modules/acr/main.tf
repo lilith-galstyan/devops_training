aa439085775c0965e03a8dc90fea1402d0bad244
Lilit <galstyan.lilit.00@gmail.com>
2026-07-06 18:31:24 +0400

    changed dockerfile path

--- task08/modules/acr/main.tf
+++ task08/modules/acr/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_container_registry_task" "acr_build_task" {
     os = "Linux"
   }
   docker_step {
-    dockerfile_path      = "application/Dockerfile"
+    dockerfile_path      = "task08/application/Dockerfile"
     context_path         = var.github_repo_url
     context_access_token = var.git_pat
     image_names          = ["${var.image_name}:latest"]
